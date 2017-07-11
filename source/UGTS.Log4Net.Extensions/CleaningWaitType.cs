336877488aa44ea7b3ecb509cded8c77fbc73fc9
Ben Siron <ben@siron.net>
2017-07-10 23:56:38 -0500

    xml docs

--- source/UGTS.Log4Net.Extensions/CleaningWaitType.cs
+++ source/UGTS.Log4Net.Extensions/CleaningWaitType.cs
@@ -1,9 +1,21 @@
 namespace UGTS.Log4Net.Extensions
 {
+    /// <summary>
+    /// The waiting strategy to use for the log directory cleaning task
+    /// </summary>
     public enum CleaningWaitType
     {
+        /// <summary>
+        /// Never wait for the task to complete - run asynchronously
+        /// </summary>
         Never,
+        /// <summary>
+        /// Only wait for the task to complete if it was run on the first logging call for the process
+        /// </summary>
         FirstTimeOnly,
+        /// <summary>
+        /// Always wait for the task to complete - run synchronously
+        /// </summary>
         Always
     }
 }

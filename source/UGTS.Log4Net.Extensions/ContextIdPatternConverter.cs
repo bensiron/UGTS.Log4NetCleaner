e94f413f4b45e988eefede86e2dd89f0d20f5561
Ben Siron <ben@siron.net>
2017-07-11 07:44:00 -0500

    use testing 1.0.3, xml docs

--- source/UGTS.Log4Net.Extensions/ContextIdPatternConverter.cs
+++ source/UGTS.Log4Net.Extensions/ContextIdPatternConverter.cs
@@ -3,8 +3,14 @@ using log4net.Util;
 
 namespace UGTS.Log4Net.Extensions
 {
+    /// <summary>
+    /// Writes an auto-increment context id which is unique per http request and managed thread
+    /// </summary>
     public class ContextIdPatternConverter : PatternConverter
     {
+        /// <summary>
+        /// Writes an auto-increment context id which is unique per http request and managed thread
+        /// </summary>
         protected override void Convert(TextWriter writer, object state)
         {
             writer.Write(Context.GetOrCreateContextId);
--- source/UGTS.Log4Net.Extensions/ExtendedPatternLayout.cs
+++ source/UGTS.Log4Net.Extensions/ExtendedPatternLayout.cs
@@ -4,9 +4,15 @@ using log4net.Util;
 
 namespace UGTS.Log4Net.Extensions
 {
+    /// <summary>
+    /// adds support for the ctxid pattern key
+    /// </summary>
     [UsedImplicitly]
     public class ExtendedPatternLayout : PatternLayout
     {
+        /// <summary>
+        /// default constructor
+        /// </summary>
         public ExtendedPatternLayout()
         {
             AddConverter(new ConverterInfo { Name = "ctxid", Type = typeof(ContextIdPatternConverter) });
